0dbdc133d3af7529a5c86a691f12496df97cd253
Duc <nguyenphungduc1807@gmail.com>
2025-06-08 08:57:49 +0700

    with digital PID

--- grad_app/Core/Src/PIDController.c
+++ grad_app/Core/Src/PIDController.c
@@ -27,3 +27,28 @@ float PID_Compute(PIDController* pid, float setpoint, float measurement) {
 
     return pid->output;
 }
+
+// //Digital PID controller implementation using incremental form
+// float PID_Compute(PIDController* pid, float setpoint, float measurement) {
+//     // Shift error history
+//     pid->error[2] = pid->error[1];
+//     pid->error[1] = pid->error[0];
+
+//     // New error
+//     pid->error[0] = setpoint - measurement;
+
+//     // PID difference equation (incremental form)
+//     float delta_output = pid->Kp * (pid->error[0] - pid->error[1])
+//                        + pid->Ki * pid->Ts * pid->error[0]
+//                        + pid->Kd / pid->Ts * (pid->error[0] - 2.0f * pid->error[1] + pid->error[2]);
+
+//     pid->output += delta_output;
+
+//     // Anti-windup
+//     if (pid->output > VOLTAGE_MAX)
+//         pid->output = VOLTAGE_MAX;
+//     else if (pid->output < -VOLTAGE_MAX)
+//         pid->output = -VOLTAGE_MAX;
+
+//     return pid->output;
+// }
